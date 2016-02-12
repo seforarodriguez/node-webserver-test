904d9d804a4228c8ad8f4b826511713c8fc39fd0
seforarodriguez <seforarodriguez13@gmail.com>
2016-02-12 15:13:20 -0600

    Changes to a working page Refactoring

--- server.js
+++ server.js
@@ -1,56 +1,45 @@
 'use strict';
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose')
+
+
+const routes = require('./routes/routes');
+const api = require('./routes/api');
+
 //This will give an alterntive in case I dont pass in a port in terminal
 const PORT = process.env.PORT || 3000;
+const MONGODB_URL = 'mongodb://localhost:27017/WEBSERVER';
 
-//http.createServer((req, res) => {
-  //console.log(req.method, req.url);
 
-  //debugger;
-  ////If the code is not with an actual html order this forces it to assume its html
+app.set('view engine', 'jade');
+app.locals.title = "The Super Cool App";
 
-//})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/hello', (req, res) => {
-  const name = req.query.name;
-  const msg =`<h1>Hello ${name}!!!!</h1>
-  <h2>Goodbye ${name}</h2>`;
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
-  console.log("query params", req.query);
+app.use(routes);
+app.use(api);
 
-  res.writeHead(200, {
-    'Content-type': 'text/html'
-  });
+mongoose.connect(MONGODB_URL);
 
-    //chunk response by character
-    msg.split('').forEach((char, i) => {
-      setTimeout (() => {
-        res.write(char);
-      }, 1000 * i);
-     });
-
-    //wait for all characters to be sent
-    setTimeout (() => {
-    res.end()
-    }, msg.length * 1000 + 2000);
- });
-
-app.get('/random/:min/:max', (req, res) => {
-  const min = req.params.min;
-  const mac = req.params.max;
-  res.end(function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+ mongoose.connection.on('open', () => {
+  console.log('MONGO OPEN!');
+  
+  app.listen(PORT, () => {
+      console.log(`Node.js server started. Listening on port ${PORT}`);
   });
 });
 
-app.all('*', (req, res) => {
-  res
-  .status(res)
-  .send('Access Denied');
-});
+module.export = app;
 
-app.listen(PORT, () => {
-    console.log(`Node.js server started. Listening on port ${PORT}`);
-});
+
+
+
+
+
